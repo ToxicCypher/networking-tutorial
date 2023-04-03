76c847f504fb4bf762262b064df717ce759127c3
ToxicCypher <129532051+ToxicCypher@users.noreply.github.com>
2023-04-03 19:51:33 -0400

    q

--- Networking/NetworkPlayer.cs
+++ Networking/NetworkPlayer.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using Unity.Netcode.Components;
 
 public class NetworkPlayer : NetworkBehaviour
 {
-
     [SerializeField] private Rigidbody rigidBody;
+
     [SerializeField] private float movementSpeed;
 
     private float yaw;
@@ -24,6 +25,10 @@ public class NetworkPlayer : NetworkBehaviour
             Camera.main.transform.position = transform.position;
             Camera.main.transform.localEulerAngles = Vector3.zero;
         }
+    }
+
+    private void Awake()
+    {
     }
 
     // Start is called before the first frame update
